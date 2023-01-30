711b1a36ee210acca8e71ae4703f273b2c82ced3
Ekaterina Ruppel <ekaterinkaa@yandex.ru>
2023-01-30 13:54:12 +0300

    задача 4

--- 01_Homework/Program.cs
+++ 01_Homework/Program.cs
@@ -1,19 +1,40 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите первое число: ");
-int num_A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число: ");
+// int num_A = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
-int num_B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num_B = Convert.ToInt32(Console.ReadLine());
 
-if (num_A > num_B)
+// if (num_A > num_B)
+// {
+//     Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+// }
+// else
+// {
+//     Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+// }
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите 3 числа:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
+int num_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num_1;
+
+if (num_2 > max)
 {
-    Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+    max = num_2;
 }
-else
+
+if (num_3 > max)
 {
-    Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+    max = num_3;
 }
 
-
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
 
