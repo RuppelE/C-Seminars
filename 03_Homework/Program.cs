87137e323c7333ea1b2f9f3f43236df95b645d53
Ekaterina Ruppel <ekaterinkaa@yandex.ru>
2023-02-06 13:09:23 +0300

    домашняя работа к 3 семинару, 21 задача

--- 03_Homework/Program.cs
+++ 03_Homework/Program.cs
@@ -1,18 +1,18 @@
-﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел  
-// от 1 до N.  System.Console.Write("Введите число N: "); 
-System.Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Таблица кубов чисел от 1 до " + n);
-if (n > 0)
-{
-    for (int i = 1; i <= n; i++)
-    {
-              System.Console.WriteLine(Math.Pow(i, 3));
-    }
+﻿// // Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел  
+// // от 1 до N.  System.Console.Write("Введите число N: "); 
+// System.Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Таблица кубов чисел от 1 до " + n);
+// if (n > 0)
+// {
+//     for (int i = 1; i <= n; i++)
+//     {
+//               System.Console.WriteLine(Math.Pow(i, 3));
+//     }
 
-    System.Console.WriteLine();
-}
-else { System.Console.WriteLine("Число N меньше единицы!"); }
+//     System.Console.WriteLine();
+// }
+// else { System.Console.WriteLine("Число N меньше единицы!"); }
 
 
 // //Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
@@ -36,3 +36,21 @@ else { System.Console.WriteLine("Число N меньше единицы!"); }
 //         Console.WriteLine("Введено некоректное значение");
 //     }
 // }
+
+//Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+System.Console.Write("Введите координату x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+Console.WriteLine("Расстояние между точками: "+ d);
