2185229c8d5fdedd2039ee27c0bfc9f28d71c011
Ekaterina Ruppel <ekaterinkaa@yandex.ru>
2023-02-06 12:00:02 +0300

    домашняя работа к 3 семинару

---
+++ 03_Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел  
+// от 1 до N.  System.Console.Write("Введите число N: "); 
+System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Таблица кубов чисел от 1 до " + (n));
+if (n > 0)
+{
+    for (int i = 1; i <= n; i++)
+    {
+              System.Console.WriteLine($"{Math.Pow(i, 3)} ");
+    }
+
+    System.Console.WriteLine();
+}
+else { System.Console.WriteLine("Число N меньше единицы!"); }
