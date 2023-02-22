4d4bc07f23979e93c3c132da19a9c5b61a1c07d6
Ekaterina Ruppel <ekaterinkaa@yandex.ru>
2023-02-22 18:03:33 +0300

    домашняя работа к 7 семинару

---
+++ 07_Homework/Program.cs
@@ -0,0 +1,157 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// double[,] GenerateMatrix(int rows, int cols)
+// {
+//     double[,] matrix = new double[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = Math.Round(new Random().NextDouble()*10 , 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(double[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// int rows = ReadInt("Введите количество строк матрицы: ");
+// int cols = ReadInt("Введите количество столбцов матрицы: ");
+// System.Console.WriteLine();
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SearchElementMatrix(int[,] matrix, int i, int j)
+{
+    if (i > matrix.GetLength(0) || j > matrix.GetLength(1) || i <= 0 || j <= 0)
+    {
+        Console.WriteLine("Элемент на данной позиции не существует");
+    }
+    else
+    {
+        Console.WriteLine($"Элемент в {i} строке и {j} столбце: {matrix[(i - 1), (j - 1)]}");
+    }
+}
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int cols = ReadInt("Введите количество столбцов матрицы: ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+
+int i = ReadInt("Введите номер строки необходимого элемента: ");
+int j = ReadInt("Введите номер столбца необходимого элемента: ");
+SearchElementMatrix(myMatrix, i, j);
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[,] GenerateMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matrix = new int[rows, cols];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rand.Next(0,10);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i, j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void ArithmeticMeanCols(int[,] matrix)
+// {
+//     double[] array = new double[matrix.GetLength(1)];
+//     double[] resultArray = new double[matrix.GetLength(1)];
+    
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         for (int i = 0; i < matrix.GetLength(0); i++)
+//         {
+//             array[j] = array[j] + matrix[i, j];
+//             resultArray[j] = Math.Round((array[j] / matrix.GetLength(0)), 1);
+            
+//         }
+//     }
+//     Console.WriteLine($"{string.Join(" \t", resultArray)}");
+// }
+
+// int rows = ReadInt("Введите количество строк матрицы: ");
+// int cols = ReadInt("Введите количество столбцов матрицы: ");
+// System.Console.WriteLine();
+// var myMatrix = GenerateMatrix(rows, cols);
+// PrintMatrix(myMatrix);
+// System.Console.WriteLine();
+// ArithmeticMeanCols(myMatrix);
